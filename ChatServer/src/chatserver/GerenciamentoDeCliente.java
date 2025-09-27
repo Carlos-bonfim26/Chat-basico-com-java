d0a5fc9c292a101ac3f431202e87f05661f256bd
carlosbonfim <carlosbonfim722@gmail.com>
2025-09-27 20:01:25 -0300

    Feat: função de listar usuarios conectados e lista de comandos de ajuda

--- ChatServer/src/chatserver/GerenciamentoDeCliente.java
+++ ChatServer/src/chatserver/GerenciamentoDeCliente.java
@@ -28,50 +28,78 @@ public class GerenciamentoDeCliente implements Runnable {
     }
 
     @Override
+    
     public void run() {
         try {
+            //inicializando os canais de comunicação
             input = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
             output = new PrintWriter(cliente.getOutputStream(), true);
             output.println("por favor coloque o seu nome:");
+            //lendo o nome do cliente
             output.flush();
             String message = input.readLine();
             output.println("Bem vindo ao chat " + message);
-            this.nomeCliente = message;
+            this.nomeCliente = message.toLowerCase().replaceAll(",", "");
+            // adicionando o cliente à lista de conectados
             clientesConectados.put(nomeCliente, this);
+            //lendo mensagens do cliente
             while (true) {
+            
                 message = input.readLine();
-                
+                //permitir que o cliente saia do chat
                 if (message.equalsIgnoreCase("::sair")) {
                     output.println("Voce saiu do chat");
                     cliente.close();
                     break;
-                } else if (message.toLowerCase().startsWith("::msg")) {
+                }
+                //listar clientes conectados
+                else if(message.trim().equalsIgnoreCase("::lista")){
+                StringBuilder str = new StringBuilder();
+                //montando a lista de clientes conectados
+                for(String c : clientesConectados.keySet()){
+                    str.append(c).append("\n");
+                }
+                output.println("Clientes conectados:\n" + str);
+                
+            }
+            //mostrar ajuda 
+            else if(message.trim().equalsIgnoreCase("::ajuda")){
+                output.println("Comandos disponíveis:\n:: <destinatario> <mensagem> - Enviar mensagem privada\n::lista - Listar clientes conectados\n::sair - Sair do chat\n::ajuda - Mostrar esta ajuda");
+            }
+              
+                // Envio de mensagens para outros clientes
+                else if (message.toLowerCase().startsWith("::")) {
 
                     String[] parts = message.split(" ");
-
+                    // Verificando se o formato da mensagem está correto
                     if (parts.length > 2) {
+                        // Extraindo o nome do destinatário e o conteúdo da mensagem
                         String nomedestinatario = parts[1];
-                        String conteudo = message.substring(6 + nomedestinatario.length()).trim();
+                        String conteudo = message.substring(3 + nomedestinatario.length()).trim();
 
                         if (conteudo.isEmpty()) {
                             output.println("Mensagem vazia, escreva algo após o destinatário!");
                         } else {
+                            // Enviando a mensagem para o destinatário
                             System.out.println("Enviando para " + nomedestinatario);
                             GerenciamentoDeCliente destinatario = clientesConectados.get(nomedestinatario);
 
                             if (destinatario == null) {
                                 output.println("Cliente " + nomedestinatario + " nao encontrado");
                             } else {
+                                // Enviando a mensagem para o destinatário
                                 destinatario.getOutput().println(getNomeCliente() + ": " + conteudo);
                                 output.println(nomeCliente + ": " + conteudo);
                             }
-                        }
+                        } 
                     } else {
-                        output.println("Formato inválido. Use: ::msg <destinatario> <mensagem>");
+                        output.println("comando inválido. Use: ::ajuda para ver os comandos disponíveis.");
                     }
 
+                }   else {
+                    output.println("Comando inválido. Use ::ajuda para ver os comandos disponíveis.");
                 }
-
+            
             }
 
         } catch (IOException ex) {
